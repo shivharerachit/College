2eaf84245498935dc2bfb8ff2092b88b5dd42ee3
Rachit Shivhare <rachitshivhare@gmail.com>
2024-05-29 23:11:40 +0530

    Added FIFO and LRU page replacement algorithms

--- OS/PageReplacementAlgorithm/CompareAll.cpp
+++ OS/PageReplacementAlgorithm/CompareAll.cpp
@@ -23,13 +23,24 @@ using namespace std;
 // No. Page Faults
 // No. of Page Replacements
 
-int nFrames, nPages, framePointer=0;
+int nFrames, nPages, framePointer=0, first[MAX_FRAMES], frequency[MAX_PAGES];
 char pageRefString[MAX_PAGES], frame[MAX_FRAMES];
+void initialise(){
+    for(int i = 0 ; i<nPages ; i++){
+        frequency[i]=0;
+    }
+}
 
 // Page Replacement Algorithms
 // 1. FIFO
 int fifo(int i){
-    
+    int x = first[0];
+    int j;
+    for(j = 0 ; j<nFrames-1 ; j++){
+        first[j] = first[j+1];
+    }
+    first[j] = x;
+    return x;
 }
 // 2. LRU
 int lru(int i){
@@ -41,7 +52,32 @@ int lru(int i){
     return -1;
 }
 // 3. LFU
+int lfu(int i){
+    int x, min = 999, j;
+    for(j = 0 ; j<nFrames ; j++) if(min<frequency[(int)frame[j] - 64]){
+        min = frequency[(int)frame[j] - 64];
+        x = j;
+    }
+    if(min == 999) x = fifo(i);
+    else{
+        for(j = x ; j<nFrames-1 ; j++){
+            first[j] = first[j+1];
+        }
+        first[j] = x;
+    }
+    frequency[pageRefString[i]]++;
+    return x;
+}
 // 4. Optimal
+int optimal(int i){
+    int j, k, max = 0;
+    for(j = 0 ; j<nFrames ; j++){
+        for(k = j+1 ; k<nFrames ; k++){
+            if(frame[j] == frame[k] && max < k-j) max = k-j;
+            else 
+        }
+    }
+}
 
 
 void input(){
@@ -53,6 +89,7 @@ void input(){
     for(int i=0 ; i<nPages ; i++){
         cin>>pageRefString[i];
     }
+    initialise();
 }
 
 bool checkPage(char page){
@@ -65,18 +102,23 @@ bool checkPage(char page){
 int* mainLogic(string algorithm){
     int pageFault = 0, pageReplacement = 0, index;
     for(int i=0 ; i<nPages ; i++){
-        if(checkPage(pageRefString[i])) continue;
+        if(checkPage(pageRefString[i])){
+            frequency[(int)pageRefString[i] - 64]++;
+            continue;
+        }
         else{
             pageFault++;
             if(framePointer<nFrames){
+                first[framePointer] = framePointer;
+                frequency[(int)pageRefString[i] - 64]++;
                 frame[framePointer]=pageRefString[i];
                 framePointer++;
             }
             else{
                 pageReplacement++;
-                if(algorithm == "FIFO") continue;//index = fifo(i);
+                if(algorithm == "FIFO") index = fifo(i);
                 else if(algorithm == "LRU") index = lru(i);
-                // else if(algorithm == "LFU") lfu();
+                else if(algorithm == "LFU") index = lfu(i);
                 // else if(algorithm == "OPTIMAL") optimal();
                 else cout<<"Wrong Algorithm.";
                 frame[index] = pageRefString[i];
@@ -90,7 +132,7 @@ int* mainLogic(string algorithm){
 }
 
 void output(){
-    int* arr = mainLogic("LRU");
+    int* arr = mainLogic("LFU");
     cout<<"\nNo. of Page Faults: "<<arr[0]<<"\n";
     cout<<"\nNo. of Page Replacements: "<<arr[1]<<"\n";
 }
